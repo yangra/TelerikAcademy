c09996400a643f93e22922ca8cae7e340c887b33
Yangra <brumbazluk@abv.bg>
2013-05-13 14:24:54 +0300

    partial hw 3

---
+++ Programming/CSharpPartOne/OperatorsAndExpressions/01.OddOrEven/OddOrEven.cs
@@ -0,0 +1,22 @@
+﻿//Write an expression that checks if given integer is odd or even.
+
+using System;
+
+class OddOrEven
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter an integer:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("This number is even!");
+        }
+        else 
+        {
+            Console.WriteLine("This number is odd!");
+        }
+    }
+}
+
---
+++ Programming/CSharpPartOne/OperatorsAndExpressions/02.DivideBy7And5/Divisible.cs
@@ -0,0 +1,17 @@
+﻿//Write a boolean expression that checks for given integer if it can be divided
+//(without remainder) by 7 and 5 in the same time.
+
+using System;
+
+class Divisible
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter an integer:");
+        int number = int.Parse(Console.ReadLine());
+
+        bool divisible = number % 5 == 0 && number % 7 == 0;
+        Console.WriteLine(divisible);
+    }
+}
+
