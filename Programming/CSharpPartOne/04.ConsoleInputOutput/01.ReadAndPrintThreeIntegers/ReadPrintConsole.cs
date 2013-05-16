19d22915e6a56f8e540ccda9a60a372b3a2e7207
Yangra <brumbazluk@abv.bg>
2013-05-16 10:01:56 +0300

    hw4

---
+++ Programming/CSharpPartOne/04.ConsoleInputOutput/01.ReadAndPrintThreeIntegers/ReadPrintConsole.cs
@@ -0,0 +1,39 @@
+﻿//Write a program that reads 3 integer numbers from the console and 
+//prints their sum.
+
+using System;
+
+class ReadPrintConsole
+{
+    static void Main()
+    {
+        int firstInteger = 0;
+        int secondInteger = 0;
+        int thirdInteger = 0;
+
+
+        Console.WriteLine("Please enter integer 1:");
+        while (!int.TryParse(Console.ReadLine(), out firstInteger))
+        {
+            Console.WriteLine("You entered invalid number! Try again! {0}Please enter integer 1:",
+                Environment.NewLine);
+        }
+        Console.WriteLine("Please enter integer 2:");
+        while (!int.TryParse(Console.ReadLine(), out secondInteger))
+        {
+            Console.WriteLine("You entered invalid number! Try again! {0}Please enter integer 2:",
+                Environment.NewLine);
+        }
+        Console.WriteLine("Please enter integer 3:");
+        while (!int.TryParse(Console.ReadLine(), out thirdInteger))
+        {
+            Console.WriteLine("You entered invalid number! Try again! {0}Please enter integer 3:",
+                Environment.NewLine);
+        }
+
+        Console.WriteLine("{0} + {1} + {2} = {3}",firstInteger,
+            secondInteger,thirdInteger,firstInteger+secondInteger+thirdInteger);
+        
+    }
+}
+
---
+++ Programming/CSharpPartOne/04.ConsoleInputOutput/02.CirclePerimeterArea/ReadRadius.cs
@@ -0,0 +1,24 @@
+﻿//Write a program that reads the radius r of a circle and prints its perimeter 
+//and area.
+
+using System;
+
+class ReadRadius
+{
+    static void Main()
+    {
+        int radius;
+        Console.WriteLine("Enter the radius of a circle:");
+        while (!int.TryParse(Console.ReadLine(), out radius)) 
+        {
+            Console.WriteLine("You entered an invalid number, please try again!");
+            Console.WriteLine("Enter the radius of a circle:");
+        }
+
+        double circlePerimeter = 2 * Math.PI * radius;
+        double circleArea = Math.PI * radius * radius;
+        Console.WriteLine("The area of the circle is: {0}", circleArea);
+        Console.WriteLine("The perimeter of the circle is: {0}", circlePerimeter);
+    }
+}
+
