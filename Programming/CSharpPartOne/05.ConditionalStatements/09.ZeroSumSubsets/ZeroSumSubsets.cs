05371163621bce568b9b5e7aa46f5e7444f5fdc0
Yangra <brumbazluk@abv.bg>
2013-05-18 23:47:15 +0300

    HW5 - Prblm9

--- Programming/CSharpPartOne/05.ConditionalStatements/09.ZeroSumSubsets/ZeroSumSubsets.cs
+++ Programming/CSharpPartOne/05.ConditionalStatements/09.ZeroSumSubsets/ZeroSumSubsets.cs
@@ -7,13 +7,54 @@ class ZeroSumSubsets
 {
     static void Main()
     {
-        int one = 3;
-        int two = -2;
-        int three = 1;
-        int four = 1;
-        int five = 8;
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Enter integer:");
+            while (!int.TryParse(Console.ReadLine(), out numbers[i])) 
+            {
+                Console.WriteLine("You entered an invalid number! Try again!");
+            }
+        }
+        int length = (int)Math.Pow(2,numbers.Length);
+        int zeroSumCount = 0;
+        int subsetSum = 0;
 
+        for (int i = 1; i < length; i++)
+        {
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                subsetSum += ((i >> j) & 1) * numbers[j];
+            }
+            if (subsetSum == 0)
+            {
+                for (int k = 0; k < numbers.Length; k++)
+                {
+                    subsetSum += ((i >> k) & 1) * numbers[k];
+                    if (((i >> k) & 1) > 0)
+                    {
+                        if (numbers[k] < 0)
+                        {
+                            Console.Write("\b" + numbers[k]+ "+");
+                        }
+                        else
+                        {
+                            Console.Write(numbers[k] + "+");
+                        }  
+                        
+                    }
 
+                }
+                Console.WriteLine("\b=0");
+                
+                zeroSumCount++;
+            }
+            subsetSum = 0;
+        }
+        if (zeroSumCount == 0)
+        {
+            Console.WriteLine("There are no subsets summing to zero!");
+        }
 
     }
 }
