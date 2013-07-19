b5afbcd2411a0481ef2609317f5dabea80e2797e
Yangra <brumbazluk@abv.bg>
2013-07-19 17:19:54 +0300

    HW 2

---
+++ Programming/CSharpPartTwo/02.MultidimentionalArrays/04.LowerBound/LowerBound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class LowerBound
+{
+    static void Main()
+    {
+        int k = 999999;
+        int[] array = { 4, 5, 1, 18, 2, 4, 12, 9, 8, 7, 13, 45, 34, 1000000 };
+        int index=0;
+        int element = 0;
+        Array.Sort(array);
+        Console.Write("Sorted array: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("{0} ", array[i]);
+        }
+        Console.WriteLine();
+        for (int i = k; i >= 0; i--)
+        {
+           index = Array.BinarySearch(array, i);
+           if (index <0)
+           {
+               continue;
+           }
+           element = array[index];
+            break;
+        }
+        Console.WriteLine("The largest number of the array less than {0} is {1} with index {2}",k,element,index);
+    }
+}
+
---
+++ Programming/CSharpPartTwo/02.MultidimentionalArrays/05.SortStringsBylength/SortStringsBylength.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class SortStringsBylength
+{
+    static void Main()
+    {
+        string[] strArray = {"abd" , "mc","tralala","console","freesbee", "face","cadet"};
+        int[] length = new int[strArray.Length];
+        for (int i = 0; i < strArray.Length; i++)
+        {
+            length[i] = strArray[i].Length;
+        }
+        Array.Sort(length,strArray);
+        Console.WriteLine(String.Join(" ", strArray));
+    }
+}
+
