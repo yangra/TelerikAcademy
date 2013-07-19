c7aa7c27ac15baae42946eed7927a04158b7526e
Yangra <brambazluk@gmail.com>
2013-07-19 18:34:58 +0300

    changed array

--- Programming/CSharpPartTwo/02.MultidimentionalArrays/05.SortStringsBylength/SortStringsBylength.cs
+++ Programming/CSharpPartTwo/02.MultidimentionalArrays/05.SortStringsBylength/SortStringsBylength.cs
@@ -4,7 +4,7 @@ class SortStringsBylength
 {
     static void Main()
     {
-        string[] strArray = {"abd" , "mc","tralala","console","freesbee", "face","cadet"};
+        string[] strArray = {"abd" , "mc","tralala","console","freesbee", "face","cadet", "mojo", "a", "alabalanica"};
         int[] length = new int[strArray.Length];
         for (int i = 0; i < strArray.Length; i++)
         {
