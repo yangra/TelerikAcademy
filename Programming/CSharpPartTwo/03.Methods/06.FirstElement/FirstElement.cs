4ce291777c78e7ba1263269f86999ca4a7e44186
Yangra <brambazluk@gmail.com>
2013-07-25 01:14:19 +0300

    Problem 6

---
+++ Programming/CSharpPartTwo/03.Methods/06.FirstElement/FirstElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class FirstElement
+{
+    static bool isBiggerThanNeightbours(int[] array, int index)
+    {
+        return index > 0 && index < array.Length - 1 && 
+                array[index] > array[index + 1] && array[index] > array[index - 1];
+    }
+
+    static int ReturnFirstIndex(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (isBiggerThanNeightbours(array, i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void Main()
+    {
+        int[] arr = { 0, 2, 3, 8, 12, 6, 5 };
+        //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        Console.WriteLine(ReturnFirstIndex(arr));
+    }
+}
+
