2f58fd82617f8fa96367fbfde622d39599de843f
Yangra <brambazluk@gmail.com>
2013-07-25 02:03:16 +0300

    Problem 7

---
+++ Programming/CSharpPartTwo/03.Methods/07.ReversedDigits/ReversedDigits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ReversedDigits
+{
+    static string ReverseDigits(string number)
+    {
+        number.ToCharArray();
+        string reversed = new string('-', number.Length);
+        char[] rev = reversed.ToCharArray();
+        for (int i = number.Length - 1, j = 0; i <= 0; i++, j++)
+        {
+            rev[j] = number[i];
+        }
+        reversed = rev.ToString();
+        return reversed;
+    }
+    static void Main()
+    {
+        Console.Write("Please enter a number: ");
+        string number = Console.ReadLine();
+        Console.WriteLine(ReverseDigits(number));
+    }
+}
+
